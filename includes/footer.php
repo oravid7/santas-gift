2a024304f0f6a8311c7ca46f2bc41485b2d25ffd
haim ravid <ravidinvest@gmail.com>
2023-10-04 09:11:28 +0300

    my new gadgets site

---
+++ includes/footer.php
@@ -0,0 +1,84 @@
+<section class="footer-gadget container gap">
+  <footer class="footer">
+    <div class="container grid grid--footer gap">
+      <div class="logo-col">
+        <a href="#" class="footer-logo">
+          <!-- <img
+            class="logo"
+            alt="omnifood logo"
+            src="img/app/omnifood-logo.png"
+          /> -->
+        </a>
+
+        <ul class="social-links">
+          <li>
+            <a class="footer-link" href="#"
+              ><ion-icon class="social-icon" name="logo-instagram"></ion-icon
+            ></a>
+          </li>
+          <li>
+            <a class="footer-link" href="#"
+              ><ion-icon class="social-icon" name="logo-facebook"></ion-icon
+            ></a>
+          </li>
+          <li>
+            <a class="footer-link" href="#"
+              ><ion-icon class="social-icon" name="logo-twitter"></ion-icon
+            ></a>
+          </li>
+        </ul>
+
+        <p class="copyright">
+          Copyright &copy; <span class="year">2027</span>
+          
+          
+          by GadgetBox,Inc.All rights reserved
+        </p>
+      </div>
+      <div class="address-col">
+        <p class="footer-heading">Contact us</p>
+        <address class="contacts">
+          <p class="address">
+            623 Harrison St., 2nd Floor, San Francisco, CA 94107
+          </p>
+          <a class="footer-link" href="tel:415-201-6370">415-201-6370</a
+          ><br />
+          <!-- <a class="footer-link" href="mailto:ravidinvest@gmail.com"
+            >ravidinvest@gmail.com</a
+          > -->
+        </address>
+      </div>
+      <nav class="nav-col">
+        <p class="footer-heading">Account</p>
+        <ul class="footer-nav">
+          <li><a class="footer-link" href="#">Create account</a></li>
+          <li><a class="footer-link" href="#">Sign in</a></li>
+          <li><a class="footer-link" href="#">ios app</a></li>
+          <li><a class="footer-link" href="#">android app</a></li>
+        </ul>
+      </nav>
+      <nav class="nav-col">
+        <p class="footer-heading">leading Categories</p>
+        <ul class="footer-nav">
+          <li><a class="footer-link" href="#">gadgets</a></li>
+          <li><a class="footer-link" href="#">sales</a></li>
+          <li><a class="footer-link" href="#">gadgets for smartphone</a></li>
+          <li><a class="footer-link" href="#">gift for home</a></li>
+          <li><a class="footer-link" href="#">gift for office</a></li>
+          <li><a class="footer-link" href="#">gadgets for kitchen</a></li>
+
+        </ul>
+      </nav>
+      <nav class="nav-col">
+        <p class="footer-heading">Resource</p>
+        <ul class="footer-nav">
+          <li><a class="footer-link" href="#">Recipe directory</a></li>
+          <li><a class="footer-link" href="#">Help center</a></li>
+          <li><a class="footer-link" href="#"> Privacy & terms </a></li>
+        </ul>
+      </nav>
+    </div>
+  </footer>
+</section>
+  </body>
+</html>
---
+++ includes/header.php
@@ -0,0 +1,35 @@
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link
+      href="https://fonts.googleapis.com/css2?family=Rubik:wght@400;500;600;700&display=swap"
+      rel="stylesheet"
+    />
+    <script
+      type="module"
+      src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"
+    ></script>
+
+    
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link
+      rel="stylesheet"
+      href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css"
+    />
+    <script defer src="js/script.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.6.4/dist/jquery.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"></script>
+    <link rel="stylesheet" href="css/reusable.css" />
+    <link rel="stylesheet" href="css/style.css" />
+    <link rel="stylesheet" href="css/queries.css" />
+    <meta name="yandex-verification" content="539c2cddbed37e13" />
+
+   
